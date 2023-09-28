8617abd663f571297669bc72c669d55b4b196b40
camilobeta11 <camilobeta11@hotmail.com>
2023-09-28 00:52:56 -0500

    config routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductListComponent } from './pages/product-list/product-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'product-list',
+    component: ProductListComponent,
+  },
+  { path: '**', redirectTo: 'product-list' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
